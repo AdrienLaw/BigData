b4de429ec39f44086ec410eeeb81f41602e80134
luohaotian <up842514938>
2020-02-23 15:32:21 +0800

    ignore

--- mr_200207/src/main/java/com/adrien/mr/outputformat/FilterDriver.java
+++ mr_200207/src/main/java/com/adrien/mr/outputformat/FilterDriver.java
@@ -35,7 +35,7 @@ public class FilterDriver {
         FileInputFormat.setInputPaths(job, new Path(args[0]));
 
         // 虽然我们自定义了outputformat，但是因为我们的outputformat继承自fileoutputformat
-        // 而fileoutputformat要输出一个_SUCCESS文件，所以，在这还得指定一个输出目录
+        // 而fileoutputformat要输出一个_SUCCESS文件，所以，在这还得指定一个输出目录 133
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
         boolean result = job.waitForCompletion(true);
