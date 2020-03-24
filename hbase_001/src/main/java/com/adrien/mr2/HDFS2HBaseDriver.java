3350947aa4a371970fa55cad8172560d3225630a
luohaotian <up842514938>
2020-03-24 20:02:02 +0800

    123

--- hbase_001/src/main/java/com/adrien/mr2/HDFS2HBaseDriver.java
+++ hbase_001/src/main/java/com/adrien/mr2/HDFS2HBaseDriver.java
@@ -1,4 +1,78 @@
 package com.adrien.mr2;
 
-public class HDFS2HBaseDriver {
+import com.adrien.mr.Fruit2FruitMRJob;
+import com.adrien.mr.WriteFruitMRReducer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import java.io.IOException;
+
+public class HDFS2HBaseDriver extends Configured implements Tool {
+
+    //创建Hadoop 以及 HBase管理配置对象
+    public static Configuration conf;
+
+    private static final String ZK_QUORUM = "hbase.zookeeper.quorum";
+    private static final String ZK_CLIENT_PORT = "hbase.zookeeper.property.clientPort";
+    private static final String HBASE_POS = "100.168.1.182";
+    private static final String ZK_POS = "Master:2181,Slave0:2181,Slave1:2181,Slave2:2181";
+    private static final String ZK_PORT_VALUE = "2181";
+
+    static{
+        //使用HBaseConfiguration的单例方法实例化
+        conf = HBaseConfiguration.create();
+        conf.set("hbase.zookeeper.quorum", "hadoop102"); // 换成自己的ZK节点IP
+        conf.set("hbase.zookeeper.property.clientPort", ZK_PORT_VALUE);
+    }
+
+    public static void main(String[] args) {
+        HDFS2HBaseDriver hdfs2HBaseDriver = new HDFS2HBaseDriver();
+        try {
+            int run = ToolRunner.run(conf, hdfs2HBaseDriver, args);
+            System.exit(run);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        //得到Configuration
+        Configuration conf = this.getConf();
+
+        //创建Job任务
+        Job job = Job.getInstance(conf, this.getClass().getSimpleName());
+        job.setJarByClass(HDFS2HBaseDriver.class);
+//    	Path inPath = new Path("/input/fruit.txt");
+        Path inPath = new Path("hdfs://hadoop102:9000/input_fruit/fruit.tsv");
+        FileInputFormat.addInputPath(job, inPath);
+
+        //设置Mapper
+        job.setMapperClass(ReadFruitFromHDFSMapper.class);
+        job.setMapOutputKeyClass(ImmutableBytesWritable.class);
+        job.setMapOutputValueClass(Put.class);
+
+        //设置Reducer
+        TableMapReduceUtil.initTableReducerJob("fruit_from_hdfs", WriteFruitMRFromTxtReducer.class, job);
+
+        //设置Reduce数量，最少1个
+        job.setNumReduceTasks(1);
+
+        boolean isSuccess = job.waitForCompletion(true);
+        if(!isSuccess){
+            throw new IOException("Job running with error");
+        }
+
+        return isSuccess ? 0 : 1;
+    }
+
 }
--- hbase_001/src/main/java/com/adrien/mr2/ReadFruitFromHDFSMapper.java
+++ hbase_001/src/main/java/com/adrien/mr2/ReadFruitFromHDFSMapper.java
@@ -1,4 +1,36 @@
 package com.adrien.mr2;
 
-public class ReadFruitFromHDFSMapper {
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class ReadFruitFromHDFSMapper extends Mapper<LongWritable, Text, ImmutableBytesWritable, Put> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        //从HDFS中读取的数据
+        String lineValue = value.toString();
+
+        String[] values = lineValue.split("\t");
+
+        String rowKey = values[0];
+        String name = values[1];
+        String color = values[2];
+
+        //初始化rowKey
+        ImmutableBytesWritable rowKeyWritable = new ImmutableBytesWritable(Bytes.toBytes(rowKey));
+
+        //初始化put对象
+        Put put = new Put(Bytes.toBytes(rowKey));
+
+        //参数分别:列族、列、值
+        put.add(Bytes.toBytes("info"), Bytes.toBytes("name"),  Bytes.toBytes(name));
+        put.add(Bytes.toBytes("info"), Bytes.toBytes("color"),  Bytes.toBytes(color));
+
+        context.write(rowKeyWritable, put);
+    }
 }
